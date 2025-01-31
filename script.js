0199456aefd33853fad3094b7edf5cd09e77d6c1
natta193 <barnes.nathnat@gmail.com>
2025-01-31 11:50:21 +0000

    init

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity to the "Coming Soon" button
+document.getElementById('comingSoonBtn').addEventListener('click', function() {
+    alert('Our first deck of language learning cards will be available soon. Stay tuned!');
+  });
